c377176305ce1f95433058122b519f071b6e002e
Osamu Ito <mr.samito@gmail.com>
2022-09-02 14:27:29 -0400

    commit tree 0b4ec7a5f60c9875e18f4496fb8675e1950a8300

---
+++ index.js
@@ -0,0 +1,14 @@
+//const divide = function() {
+//  return 2000/100}   This can be written as an arrow function, as its very simple/1 line return
+
+const divide = () => 2000/100;
+
+const square = x => x*x; 
+// syntax for this is const square = function (x) {
+//   return x*x
+// }
+
+const add = (x,y) => x+y
+// syntax for this is const add = function (x,y) {
+//   return x + y
+// }
